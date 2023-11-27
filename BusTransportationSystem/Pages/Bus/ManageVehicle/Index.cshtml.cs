089e1fa64ba3027e40ed5e95242fbebe0f09299a
josephus-mupanda <josephusmupanda48@gmail.com>
2023-11-27 11:42:35 +0200

    All CRUD opeartions about Vehicle.Don't forget to change the connString

---
+++ BusTransportationSystem/Pages/Bus/ManageVehicle/Index.cshtml.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace BusTransportationSystem.Pages.Bus.ManageVehicle
+{
+    public class IndexModel : PageModel
+    {
+        string connString = "Data Source=JOSEPHUS-ML;Initial Catalog=BusSystem;Integrated Security=True;Encrypt=False";
+
+        public List<Vehicle> VehicleList = new List<Vehicle>();
+
+        public void OnGet()
+        {
+            // Display the list of Vehicles
+            VehicleList.Clear();
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connString))
+                {
+                    // Retrieve Vehicles
+                    string vehicleQuery = "SELECT * FROM Vehicle";
+                    con.Open();
+                    using (SqlCommand vehicleCmd = new SqlCommand(vehicleQuery, con))
+                    {
+                        using (SqlDataReader vehicleReader = vehicleCmd.ExecuteReader())
+                        {
+                            while (vehicleReader.Read())
+                            {
+                                Vehicle vehicle = new Vehicle
+                                {
+                                    VehicleId = vehicleReader.GetInt32(0),
+                                    VehicleName = vehicleReader.GetString(1),
+                                    DriverId = vehicleReader.GetInt32(2),
+                                    VehicleTypes = vehicleReader.GetString(3),
+                                    Seats = vehicleReader.GetInt32(4)
+                                };
+                                VehicleList.Add(vehicle);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error" + ex.Message);
+            }
+        }
+    }
+
+    public class Vehicle
+    {
+        public int VehicleId { get; set; }
+        public string VehicleName { get; set; }
+        public int DriverId { get; set; }
+        public string VehicleTypes { get; set; }
+        public int Seats { get; set; }
+    }
+}
